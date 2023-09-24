b1e180d810453311cc6fc1aff73936dfa7af0419
Дмитрий Заранкович <98533141+beddeb@users.noreply.github.com>
2023-09-25 01:03:10 +0300

    Create Курс1-Занятие2-СмР2.2.c

---
+++ Курс1-Занятие2-СмР2.2.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<limits.h>
+
+long int ssh(int i0, int temp){
+    long int temp_l=temp;
+    for (int i=1; i<i0; i++){
+        if (temp_l <= LONG_MAX/temp) temp_l *= temp;
+        else return -1;
+    }
+    return temp_l;
+}
+
+long int main(void){
+    long int a, total=0, tot=0;
+    int k, n=0, flag=1;
+    scanf("%d", &k);
+    while (flag){
+        n++;
+        for (int i=1; i<=k; i++){
+            a = ssh(n, i);
+            if (a == -1){
+                printf("%ld\n", tot);
+                printf("%d\n", n-1);
+                printf("*\n");
+                flag = 0;
+                break;
+            }
+            if (a <= LONG_MAX - total) total += a;
+            else{
+                printf("%ld\n", tot);
+                printf("%d\n", n-1);
+                printf("+\n");
+                flag = 0;
+                break;
+            }
+        }
+        tot = total;
+        total=0;
+    }
+}
